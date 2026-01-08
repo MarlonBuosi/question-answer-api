7414d999ea31170e66af0989b0febebacefdc523
Marlon Buosi <marlonbuosi_@hotmail.com>
2026-01-07 23:11:40 -0300

    feat: add database connection with prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common'
+import { Get, Post } from '@nestjs/common'
 // biome-ignore lint/style/useImportType: <will break>
 import { AppService } from './app.service'
+import type { PrismaService } from './prisma/prisma.service'
 
-@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Post('/hello')
+  async store() {
+    return await this.prisma.users.findMany()
   }
 }
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common'
+import { PrismaClient } from 'generated/prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      accelerateUrl: process.env.PRISMA_ACCELERATE_URL || '',
+      log: ['warn', 'error'],
+    })
+  }
+  onModuleInit() {
+    return this.$connect()
+  }
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
